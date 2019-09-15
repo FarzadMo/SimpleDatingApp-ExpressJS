2ac21d17d1754cc8240e0a3c50171f4758892116
Fred M <farzad.moghbel@gmail.com>
2019-09-15 16:44:05 -0400

    the app is functional

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friendsArray = [{
+        name: 'Adam',
+        photo: 'https://images.unsplash.com/photo-1506919258185-6078bba55d2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=815&q=80',
+        scores: [5, 4, 4, 4, 3, 4, 4, 4, 4, 5]
+    },
+    {
+        name: 'Loki',
+        photo: 'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        scores: [3, 4, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: 'Sohi',
+        photo: 'https://images.unsplash.com/photo-1519742866993-66d3cfef4bbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=281&q=80',
+        scores: [2, 3, 2, 3, 3, 4, 2, 2, 3, 5]
+    },
+    {
+        name: 'Michael',
+        photo: 'https://images.unsplash.com/photo-1492567291473-fe3dfc175b45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=331&q=80',
+        scores: [2, 3, 2, 2, 4, 2, 2, 3, 2, 1]
+    },
+    {
+        name: 'Sarah',
+        photo: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+        scores: [2, 1, 3, 3, 5, 3, 2, 1, 3, 2]
+    }
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -5,23 +5,20 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({
-    type: 'application/vnd.api+json'
-}));
-app.use(express.static(path.join(__dirname, 'public')));
 
-require('./routing/apiRoutes')(app);
-require('./routing/htmlRoutes')(app);
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public'));
-});
-
+// Start listening on PORT
 app.listen(PORT, function () {
-    console.log('Listening on port: ' + PORT);
+    console.log('Friend Finder app is listening on PORT: ' + PORT);
 });
